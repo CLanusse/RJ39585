56486d942d97a55d116f3bbf3fc1218fed192669
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-02-08 21:47:45 -0300

    Clase 1 - Instalacion

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos a React JS
         </p>
+        <h4>Profe Conrado Lanusse</h4>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +20,14 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <section>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </section>
     </div>
   );
 }
