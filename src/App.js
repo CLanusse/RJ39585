531cac471772997429fc409b21a6321d422e8300
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-02-13 22:05:29 -0300

    JSX y compilacion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const saludar = () => null
+
 function App() {
+
+  const profesor = 'Conrado Lanusse'
+  const tutor = 'Santiago Rivarola'
+
+  const estilos = {
+    color: 'red',
+    marginTop: '20px'
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={estilos}>
           Bienvenidos a React JS
         </p>
-        <h4>Profe Conrado Lanusse</h4>
+        <hr/>
+        <h4>Profe { profesor }</h4>
+        <h5>{ saludar() }</h5>
         <a
           className="App-link"
           href="https://reactjs.org"
