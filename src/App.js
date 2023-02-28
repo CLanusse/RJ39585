0bc04537e87a2e96ef2e675d2c6da394b39e8f4a
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-02-27 22:02:22 -0300

    Promise y map

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import { Navbar } from "./components/Navbar/Navbar";
-import Contador from "./ejemplos/Contador/Contador";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from "react";
 
 function App() {
-  const [show, setShow] = useState(true)
-
-  const handleShow = () => {
-    setShow(!show)
-  }
 
   return (
     <div>
       <Navbar />
 
-      <button onClick={handleShow} className="btn btn-danger">Mostrar contador</button>
-
-      {
-        show ? <Contador /> : null
-      }
-
-      <ItemListContainer greeting="Hola mundo" />
+      <ItemListContainer />
 
     </div>
   );
