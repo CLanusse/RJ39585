73ea3406d8306ef095849c0ce5a43e33fb1b2329
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-03-22 20:56:53 -0300

    Firebase 2

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,10 +1,12 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext()
 
 
+const init = JSON.parse(localStorage.getItem('carrito')) || []
+
 export const CartProvider = ( {children} ) => {
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState(init)
   
     const agregarAlCarrito = (item) => {
       setCart([...cart, item])
@@ -29,6 +31,10 @@ export const CartProvider = ( {children} ) => {
     const eliminarDelCarrito = (id) => {
         setCart( cart.filter((prod) => prod.id !== id) )
     }
+
+    useEffect(() => {
+      localStorage.setItem('carrito', JSON.stringify(cart))
+    }, [cart])
 
     return (
         <CartContext.Provider value={{
